81439241c8e6349d414bca155a88e4067be1a18a
studyandup <837909535@qq.com>
2021-09-15 01:51:27 +0800

    nothing

--- CartPole_DQN/DQN.py
+++ CartPole_DQN/DQN.py
@@ -12,11 +12,11 @@ import torch.nn.functional as F
 import numpy as np
 import gym
 
-# 1. Define some Hyper Parameters
+# 1. 定义超参数
 BATCH_SIZE = 32  # batch size of sampling process from buffer
-LR = 0.01  # learning rate
+LR = 0.01     # 学习率
 EPSILON = 0.9  # epsilon used for epsilon greedy approach
-GAMMA = 0.9  # discount factor
+GAMMA = 0.9  # 衰减值
 TARGET_NETWORK_REPLACE_FREQ = 100  # How frequently target netowrk updates
 MEMORY_CAPACITY = 2000  # The capacity of experience replay buffer
 
@@ -24,8 +24,8 @@ env = gym.make("CartPole-v0")  # Use cartpole game as environment
 env = env.unwrapped
 N_ACTIONS = env.action_space.n  # 2 actions
 N_STATES = env.observation_space.shape[0]  # 4 states
-ENV_A_SHAPE = 0 if isinstance(env.action_space.sample(),
-                              int) else env.action_space.sample().shape  # to confirm the shape
+# ？
+ENV_A_SHAPE = 0 if isinstance(env.action_space.sample(),int) else env.action_space.sample().shape  # to confirm the shape
 
 
 # 2. Define the network used in both target net and the net for training
@@ -33,8 +33,11 @@ class Net(nn.Module):
     def __init__(self):
         # Define the network structure, a very simple fully connected network
         super(Net, self).__init__()
-        # Define the structure of fully connected network
+        # Define the structure of fully connected network 全连接网络
+        # https://blog.csdn.net/m0_37586991/article/details/87861418
+        # 此处设置了两个全连接层，随机权重
         self.fc1 = nn.Linear(N_STATES, 10)  # layer 1
+        # 将tensor用均值为0和标准差为0.1的正态分布填充。
         self.fc1.weight.data.normal_(0, 0.1)  # in-place initilization of weights of fc1
         self.out = nn.Linear(10, N_ACTIONS)  # layer 2
         self.out.weight.data.normal_(0, 0.1)  # in-place initilization of weights of fc2
@@ -51,6 +54,7 @@ class Net(nn.Module):
 class DQN(object):
     def __init__(self):
         # -----------Define 2 networks (target and training)------#
+        # 定义目标网络和评估网络
         self.eval_net, self.target_net = Net(), Net()
         # Define counter, memory size and loss function
         self.learn_step_counter = 0  # count the steps of learning process
@@ -58,17 +62,23 @@ class DQN(object):
 
         # ----Define the memory (or the buffer), allocate some space to it. The number
         # of columns depends on 4 elements, s, a, r, s_, the total is N_STATES*2 + 2---#
+        # s
+        # Num  Observation                Min                      Max
+        # 0     Cart Position             -4.8                    4.8
+        # 1     Cart Velocity             -Inf                    Inf
+        # 2     Pole Angle                -0.418 rad(-24 deg      0.418 rad (24 deg)
+        # 3     Pole Angular Velocity     -Inf                    Inf
         self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + 2))
 
-        # ------- Define the optimizer------#
+        # ------- Define the optimizer 优化器------#
         self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
 
-        # ------Define the loss function-----#
+        # ------Define the loss function 均方损失函数-----#
         self.loss_func = nn.MSELoss()
 
     def choose_action(self, x):
         # This function is used to make decision based upon epsilon greedy
-
+        # ？？？？？？？？？？？？？？？？？？？  np.stack()  torch.unsqueeze()
         x = torch.unsqueeze(torch.FloatTensor(x), 0)  # add 1 dimension to input state x
         # input only one sample
         if np.random.uniform() < EPSILON:  # greedy
@@ -77,15 +87,21 @@ class DQN(object):
             # print(torch.max(actions_value, 1))
             # torch.max() returns a tensor composed of max value along the axis=dim and corresponding index
             # what we need is the index in this function, representing the action of cart.
+            # torch.max(input, dim) 函数
+            # input是softmax函数输出的一个tensor
+            # dim是max函数索引的维度0/1，0是每列的最大值，1是每行的最大值
+            # 函数会返回两个tensor，第一个tensor是每行的最大值；第二个tensor是每行最大值的索引。
             action = torch.max(actions_value, 1)[1].data.numpy()
             action = action[0] if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)  # return the argmax index
         else:  # random
+            # numpy.random.randint(low, high=None, size=None, dtype='l')
             action = np.random.randint(0, N_ACTIONS)
             action = action if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)
         return action
 
     def store_transition(self, s, a, r, s_):
         # This function acts as experience replay buffer
+        # np.hstack():在水平方向上平铺 np.vstack():水平(按列顺序)把数组给堆叠起来
         transition = np.hstack((s, [a, r], s_))  # horizontally stack these vectors
         # if the capacity is full, then use index to replace the old memory with new one
         index = self.memory_counter % MEMORY_CAPACITY
@@ -97,18 +113,23 @@ class DQN(object):
         # when and how to update parameters of target network, and how to implement
         # backward propagation.
 
-        # update the target network every fixed steps
+        # update the target network every fixed steps 运行既定步数后更新目标网络
         if self.learn_step_counter % TARGET_NETWORK_REPLACE_FREQ == 0:
             # Assign the parameters of eval_net to target_net
             self.target_net.load_state_dict(self.eval_net.state_dict())
         self.learn_step_counter += 1
 
         # Determine the index of Sampled batch from buffer
+        # numpy.random.choice(a, size=None, replace=True, p=None)
         sample_index = np.random.choice(MEMORY_CAPACITY, BATCH_SIZE)  # randomly select some data from buffer
         # extract experiences of batch size from buffer.
         b_memory = self.memory[sample_index, :]
         # extract vectors or matrices s,a,r,s_ from batch memory and convert these to torch Variables
         # that are convenient to back propagation
+        # Variable是对Tensor的一个封装，操作和Tensor是一样的，
+        # 但是每个Variable都有三个属性，Varibale的Tensor本身的.data，对应Tensor的梯度.grad，
+        # 以及这个Variable是通过什么方式得到的.grad_fn。
+        # torch.FloatTensor 类型转换, 将list ,numpy转化为tensor
         b_s = Variable(torch.FloatTensor(b_memory[:, :N_STATES]))
         # convert long int type to tensor
         b_a = Variable(torch.LongTensor(b_memory[:, N_STATES:N_STATES + 1].astype(int)))
@@ -116,18 +137,21 @@ class DQN(object):
         b_s_ = Variable(torch.FloatTensor(b_memory[:, -N_STATES:]))
 
         # calculate the Q value of state-action pair
+        # gather() https://blog.csdn.net/edogawachia/article/details/80515038
         q_eval = self.eval_net(b_s).gather(1, b_a)  # (batch_size, 1)
         # print(q_eval)
         # calculate the q value of next state
+        # detach() 返回一个tensor变量，且这个变量永远不会有梯度值。这个变量跟原图上的变量共享一块内存，也就说是同一个家伙。
         q_next = self.target_net(b_s_).detach()  # detach from computational graph, don't back propagate
         # select the maximum q value
-        # print(q_next)
         # q_next.max(1) returns the max value along the axis=1 and its corresponding index
         q_target = b_r + GAMMA * q_next.max(1)[0].view(BATCH_SIZE, 1)  # (batch_size, 1)
         loss = self.loss_func(q_eval, q_target)
-
+        # 梯度初始化为零
         self.optimizer.zero_grad()  # reset the gradient to zero
+        # 反向传播求梯度
         loss.backward()
+        # 更新所有参数
         self.optimizer.step()  # execute back propagation for one step
 
 
--- numpyTest.py
+++ numpyTest.py
@@ -5,6 +5,7 @@
 # @file numpyTest.py
 
 import numpy as np
+import torch
 from pandas import DataFrame
 
 '''
@@ -24,8 +25,55 @@ from pandas import DataFrame
 # a=[1,2,3,4]
 # print(a.index(3))
 # print(np.random.uniform(0,1,(1,2)))
-df = DataFrame(np.arange(10).reshape(2,5))
-print(df)
-print('\n')
-print(df.iloc[[0]])
-print(df[0][1])
+# df = DataFrame(np.arange(10).reshape(2,5))
+# print(df)
+# print('\n')
+# print(df.iloc[[0]])
+# print(df[0][1])
+# -------------------------
+# memory = np.ones((10, 10))
+# index = np.random.choice(10, 3)
+# test = memory[index, :]
+# # print(memory)
+# print(index)
+# print(test)
+# print(test[0])
+# -----------------------
+# t = torch.Tensor([[1,2],[3,4]])
+# m = t.gather(1, torch.LongTensor([[0,0],[1,0]]))
+# print(t)
+# print(m)
+
+q_next = torch.tensor([[0.1454, -0.1610],
+                       [ 0.1349, -0.1114],
+                       [ 0.1475, -0.1558],
+                       [ 0.1443, -0.1535],
+                       [ 0.1424, -0.1404],
+                       [ 0.1415, -0.1279],
+                       [ 0.1446, -0.1467],
+                       [ 0.1404, -0.1354],
+                       [ 0.1429, -0.1422],
+                       [ 0.1420, -0.1352],
+                       [ 0.1458, -0.1543],
+                       [ 0.1376, -0.1239],
+                       [ 0.1453, -0.1635],
+                       [ 0.1371, -0.1313],
+                       [ 0.1451, -0.1570],
+                       [ 0.1447, -0.1592],
+                       [ 0.1389, -0.1326],
+                       [ 0.1377, -0.1242],
+                       [ 0.1427, -0.1461],
+                       [ 0.1409, -0.1428],
+                       [ 0.1446, -0.1545],
+                       [ 0.1424, -0.1440],
+                       [ 0.1403, -0.1353],
+                       [ 0.1384, -0.1267],
+                       [ 0.1456, -0.1645],
+                       [ 0.1426, -0.1571],
+                       [ 0.1442, -0.1441],
+                       [ 0.1465, -0.1602],
+                       [ 0.1452, -0.1644],
+                       [ 0.1455, -0.1557],
+                       [ 0.1425, -0.1454],
+                       [ 0.1449, -0.1657]])
+print(q_next.max(1)[0].view(32, 1))
